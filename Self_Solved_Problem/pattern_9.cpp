9388392b92d7d08fca426dd99b069ecff0c4b32a
MD. ABU YOUSUF <sojibhp.180640@s.pust.ac.bd>
2023-10-19 12:07:38 +0600

    Add files via upload
    Printing Triangle here.

---
+++ Self_Solved_Problem/pattern_9.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,i,j;
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=1;j<=n;j++)
+        {
+            if(i==j || i==n || j==1)
+            {
+                cout<<"*";
+            }
+            else
+            {
+                cout<<" ";
+            }
+        }
+        cout<<endl;
+    }
+}
